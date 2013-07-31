bab40b16e89cfb20185e42bebc3bcdc565cb8872
Ping Xiang <eraser117xp@gmail.com>
2013-07-31 16:49:07 -0700

    add tables

---
+++ music-shop/src/music_shop_tables.sql
@@ -0,0 +1,17 @@
+
+create table Item ();
+
+create table LeadSinger();
+
+create table HasSong();
+
+create table Purchase();
+
+create table PurchaseItem();
+
+create table Customer();
+
+create table Return();
+
+create table ReturnItem();
+

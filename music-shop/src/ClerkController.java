0eb48da4cc2ddbfc4ebb16db2c6d250ddc7a8375
Ping Xiang <eraser117xp@gmail.com>
2013-07-31 20:44:51 -0700

    add classes related to the clerk operations

---
+++ music-shop/src/ClerkController.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*; 
+import java.sql.*;
+
+
+/*
+ * ClerkController is a control class that handles action events on the Clerk menu.
+ * It updates the GUI based on which menu item the clerk selected and also use methods
+ * in ClerkModel to update the tables in database
+ */
+
+public class ClerkController {
+	
+}
---
+++ music-shop/src/ClerkModel.java
@@ -0,0 +1,23 @@
+import java.sql.*; 
+import javax.swing.event.EventListenerList;
+
+
+
+/* provides methods to carry out the actual transactions performed by a clerk:
+ * 
+ * 1. process a purchase of items in the store
+ * 2. process a return of an item fro refund
+ * 
+ * This class provides interface for the database operations (like insert, update 
+ * tables..) that are related to the above transactions. It doesn't know anything
+ * about the user interface. A ClerkController class will act as a glue between the
+ * GUI and ClerkModel
+ */
+
+public class ClerkModel {
+	
+	
+	
+	
+	
+}

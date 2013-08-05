f1508dea9dac120348624ef8ea31681f7704c3e9
joycelynn <jingyi.yan6567@gmail.com>
2013-08-04 20:05:04 -0700

    Update music_shop_tables.sql

--- music-shop/src/music_shop_tables.sql
+++ music-shop/src/music_shop_tables.sql
@@ -140,24 +140,92 @@ values(11115, 'Beyonce');
 insert into HasSong
 values(11115, 'Until the End of Time');
 
+insert into Item
+values(11116, 'Pacific Rim', 'DVD', 'pop', 'Sony', '2013', 10, 100);
+insert into LeadSinger
+values(11116, 'Ramin Djawadi');			
+insert into HasSong
+values(11116, 'Pacific Rim');	
+insert into HasSong
+values(11116, 'Gipsy Danger');	
+insert into HasSong
+values(11116, 'Just a Memory');	
+insert into HasSong
+values(11116, 'Better than New');	
+
 insert into Customer
-values('Andy', 'Andypw', 'Andy Lau', '1234 Main Mall', 6048221111)
+values('Andy', 'Andypw', 'Andy Lau', '1234 Main Mall', 6048221111);
 insert into Customer
-values('Hebe', 'Hebepw', 'Hebe T', '1221 Main Mall', 6048220000)
+values('Hebe', 'Hebepw', 'Hebe T', '1221 Main Mall', 6048220000);
 insert into Customer
-values('Tony', 'Tonypw', 'Tony Leung', '1200 Main Mall', 6048222222)
+values('Tony', 'Tonypw', 'Tony Leung', '1200 Main Mall', 6048222222);
+insert into Customer
+values('Jone', 'Jonepw', 'Jone Ross', '1200 Main Mall', 6048223333);
+insert into Customer
+values('Rachel', 'Rachelpw', 'Rachel Green', '1204 Main Mall', 6048224444);
+insert into Customer
+values('chandler', 'cpw', 'Chandler Bing', '1205 Main Mall', 6048225555);
 
 insert into Purchase
 values('A123456789', '2013-08-01', null, 1234123412341234, '2014-01-01', null, null);	
 insert into PurchaseItem
 values('A123456789', 11111, 1);	
-
 insert into Purchase
 values('A111111111', '2013-08-02', null, null, null, null, null);	
 insert into PurchaseItem
 values('A111111111', 11111, 1);	
+insert into PurchaseItem
+values('A111111111', 11112, 1);	
 
 insert into Purchase
 values('A111111112', '2013-07-02', 'Tony', 1234123412341111, '2014-03-03', '2013-07-06', '2013-07-05');	
 insert into PurchaseItem
-values('A111111112', 11112, 2);	
+values('A111111112', 11112, 2);		
+insert into PurchaseItem
+values('A111111112', 11115, 1);	
+insert into PurchaseItem
+values('A111111112', 11113, 1);
+
+insert into Purchase
+values('A111111113', '2013-08-01', null, 1234123412341222, '2015-01-01', null, null);	
+insert into PurchaseItem
+values('A111111113', 11111, 1);	
+insert into PurchaseItem
+values('A111111113', 11112, 1);	
+
+insert into Purchase
+values('A111111114', '2013-08-02', null, null, null, null, null);	
+insert into PurchaseItem
+values('A111111114', 11111, 1);	
+insert into PurchaseItem
+values('A111111114', 11112, 1);
+insert into Purchase
+values('A111111115', '2013-08-02', null, null, null, null, null);	
+insert into PurchaseItem
+values('A111111115', 11111, 1);	
+insert into PurchaseItem
+values('A111111115', 11115, 1);
+insert into Purchase
+values('A111111116', '2013-08-02', null, null, null, null, null);	
+insert into PurchaseItem
+values('A111111116', 11111, 1);	
+insert into PurchaseItem
+values('A111111116', 11112, 1);
+insert into PurchaseItem
+values('A111111111', 11113, 1);	
+insert into PurchaseItem
+values('A111111111', 11115, 1);
+
+	
+insert into Return
+values('R111111112', '2013-07-22', 'A111111112');
+insert into ReturnItem
+values('R111111112', 11112, 1);	
+insert into Return
+values('R111111113', '2013-08-02', 'A111111113');
+insert into ReturnItem
+values('R111111113', 11112, 1);
+insert into Return
+values('R111111114', '2013-08-05', 'A111111113');
+insert into ReturnItem
+values('R111111114', 11111, 1);
